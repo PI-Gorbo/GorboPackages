541aea741158104ae16b92c97bcc59c91a9db145
Gorbo <sam.I.gorbatov@gmail.com>
2024-10-29 18:35:00 +1100

    (index) Updated package index - IdentityEndpoints incremented by Pat to 1.0.1

--- GP.IdentityEndpoints/Utils.fs
+++ GP.IdentityEndpoints/Utils.fs
@@ -1,33 +1,47 @@
 ﻿namespace GP.IdentityEndpoints
 
 open System.Threading.Tasks
+open FluentValidation.Results
 open FsToolkit.ErrorHandling
 open FsToolkit.ErrorHandling.Operator.TaskResult
 open Microsoft.AspNetCore.Identity
 
 module Utils =
-    
+
     let check<'T, 'E> (check: 'T -> TaskResult<unit, 'E>) (taskResult: TaskResult<'T, 'E>) =
         taskResult
-        >>= fun value ->
-            check value 
-            |> TaskResult.map (fun _ -> value)
-    
-    
-    let ensure<'T,'E> (predicate: 'T -> bool) (errorMap: 'T -> 'E) (taskResult: TaskResult<'T, 'E>) =
+        >>= fun value -> check value |> TaskResult.map (fun _ -> value)
+
+
+    let ensure<'T, 'E> (predicate: 'T -> bool) (errorMap: 'T -> 'E) (taskResult: TaskResult<'T, 'E>) =
         taskResult
         >>= fun res ->
-                if predicate(res) then
+                if predicate (res) then
                     taskResult
                 else
-                    TaskResult.error (errorMap(res))
-        
-    
+                    TaskResult.error (errorMap (res))
+
+
     let mapIdentityResult (res: Task<IdentityResult>) : Task<Result<unit, string>> =
         res
         |> TaskResult.ofTask
-        |> ensure (fun res -> res.Succeeded) (fun res ->
-                        res.Errors
-                        |> Seq.map (fun x -> x.Description)
-                        |> String.concat ", ")
+        |> ensure
+            (fun res -> res.Succeeded)
+            (fun res ->
+                res.Errors
+                |> Seq.map (fun x -> x.Description)
+                |> String.concat ", ")
         |> TaskResult.ignore
+
+    let mapValidationResult (taskRes: Task<ValidationResult>) =
+        taskRes
+        |> TaskResult.ofTask
+        |> TaskResult.bind (fun res ->
+            if res.IsValid then
+                TaskResult.ok ()
+            else
+                TaskResult.error (
+                    res.Errors
+                    |> Seq.map (fun err -> err.ErrorMessage)
+                    |> (fun errors -> String.concat ", ")
+                ))
